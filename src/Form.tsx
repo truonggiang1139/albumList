a2b83c83470a049bfc884155762448fa7bbda0bb
Hoang Giang <truonggiang1139@gmail.com>
2023-04-22 22:03:42 +0700

    fixed error

--- src/Form.tsx
+++ src/Form.tsx
@@ -22,28 +22,30 @@ export default function Form() {
     dispatch(resetAlbum());
   };
 
-  const check=(id:number)=>{
-    if (modifyItem.id===id && isModify===true){
+  const check = (id: number) => {
+    if (modifyItem.id === id && isModify === true) {
       return true;
     }
-    return false
+    return false;
+  };
 
-  }
-  const getPhotos = () => {
-    setTimeout(async () => {
-      const res = await axios.get(
-        `https://jsonplaceholder.typicode.com/photos?&_start=${page}&_end=${
-          page + 6
-        }`
-      );
-
-      dispatch(setAlbum(res.data));
-    }, 500);
-    setPage((prev) => prev + 4);
+  const handleLoadMore = () => {
+    setPage((prev) => prev + 6);
   };
   useEffect(() => {
-    getPhotos();
-  }, []);
+    const getData = () => {
+      setTimeout(async () => {
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/photos?&_start=${page}&_end=${
+            page + 6
+          }`
+        );
+
+        dispatch(setAlbum(res.data));
+      }, 500);
+    };
+    getData();
+  }, [dispatch, page]);
   return (
     <div className=" mt-16 w-1/2">
       <div className="btn">
@@ -55,7 +57,7 @@ export default function Form() {
           Update
         </button>
         <button
-          disabled={modifyItem.id===-1}
+          disabled={modifyItem.id === -1}
           onClick={handleResetList}
           className="hover:cursor-pointer rounded px-3 mr-3 bg-blue-500 text-white cursor-default disabled:bg-gray-600 disabled:hover:cursor-auto"
         >
@@ -70,7 +72,7 @@ export default function Form() {
       {!loading && (
         <InfiniteScroll
           dataLength={albumList.length}
-          next={getPhotos}
+          next={handleLoadMore}
           hasMore={true}
           loader={<Skeleton />}
         >
